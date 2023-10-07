3d0bbff013f1358dd309f81c7afdaaac5e72403d
MatheusCuba <matheus.cuba@partner.bmw.com.br>
2023-10-06 23:29:47 -0300

    Changed index

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-// index.js
-const express = require('express')
-
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
 const app = express()
-const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+app.use(cors)
+
+app.get('/', async (req, res) => {
+	try{
+        if(!req.query.username){
+            res.send('Hello :)');
+            return;
+        }
+		let data = await axios.get(`https://www.instagram.com/${req.query.username}/channel/?__a=1&__d=1`);
+		let imageUrl = data.data.graphql.user.profile_pic_url_hd; 	
+		let imageBuffer = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+		res.type("image/jpeg");
+		res.write(imageBuffer.data)
+	} catch(e){
+		console.log('eeee', e);
+		res.send(e);
+	}
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+app.listen(4000, () => {
+	console.log(`Example app listening on port 4000`)
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
-
-// Export the Express API
-module.exports = app
+module.exports = app;

cfd9730de6d8acd7d3eff4c206f6335e44fb9ce4
MatheusCuba <matheus.cuba@partner.bmw.com.br>
2023-10-06 23:30:50 -0300

    Remove Cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express()
 
-app.use(cors)
+// app.use(cors)
 
 app.get('/', async (req, res) => {
 	try{
